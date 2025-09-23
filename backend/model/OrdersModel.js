145c4d433f29fe4b1bc13a4f584f065d7fed205d
Spandan More <spandanmore192003@gmail.com>
2025-09-23 19:51:47 +0530

    Update OrdersModel.js

--- backend/model/OrdersModel.js
+++ backend/model/OrdersModel.js
@@ -4,4 +4,4 @@ const { OrdersSchema } = require("../schemas/OrdersSchema");
 
 const OrdersModel = new model("order", OrdersSchema);
 
-module.exports = { OrderModel }
+module.exports = { OrdersModel }
